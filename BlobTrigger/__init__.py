97818a0f174d505f4dbe55f0e1d3a44318094801
ani0989 <11317519+ani0989@users.noreply.github.com>
2021-03-26 15:34:53 -0700

    First

---
+++ BlobTrigger/__init__.py
@@ -0,0 +1,13 @@
+import logging
+import datetime
+import azure.functions as func
+
+
+def main(myblob: func.InputStream):
+    timestamp = datetime.datetime.utcnow()
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes\n"
+                 f"Blob Content: {myblob.read()}"
+                 )
+    return 'Message created at: {}'.format(timestamp)
